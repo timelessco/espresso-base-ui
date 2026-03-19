29eda88434722b7c919f4b2ea7b369a9e6db8e68
padmanathan <padmanathandev@gmail.com>
2026-03-19 23:46:12 +0530

    fix(ui): prevent unwanted body scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         inter.variable
       )}
     >
-      <body>
+      <body className="overflow-hidden">
         <ThemeProvider>
           <TooltipProvider>{children}</TooltipProvider>
         </ThemeProvider>
