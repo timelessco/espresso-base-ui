0cdba2dbe91b380807ece793c94fa6854de1c90e
padmanathan <padmanathandev@gmail.com>
2026-04-30 14:42:53 +0530

    data grid table ui fix

--- components/ui-radix/select.tsx
+++ components/ui-radix/select.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from "lucide-react"
+import { CheckIcon, ChevronDownIcon } from "lucide-react"
 import { Select as SelectPrimitive } from "radix-ui"
 
 import { cn } from "@/lib/utils"
@@ -37,7 +37,7 @@ function SelectTrigger({
       data-slot="select-trigger"
       data-size={size}
       className={cn(
-        "flex w-fit items-center justify-between gap-2 rounded-md border border-border bg-primary-foreground px-3 py-2 text-sm text-secondary-foreground whitespace-nowrap shadow-xs transition-[color,box-shadow] outline-none not-disabled:hover:border-border-normal not-disabled:active:border-border-strong focus-visible:bg-secondary focus-visible:text-secondary-foreground focus-visible:shadow-3xs disabled:cursor-not-allowed disabled:bg-input disabled:text-popover-foreground aria-invalid:border-destructive aria-invalid:ring-destructive/20 data-[placeholder]:text-card-foreground data-[size=default]:h-9 data-[size=sm]:h-8 *:data-[slot=select-value]:line-clamp-1 *:data-[slot=select-value]:flex *:data-[slot=select-value]:items-center *:data-[slot=select-value]:gap-2 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 [&_svg:not([class*='text-'])]:text-muted-foreground",
+        "flex w-fit items-center justify-between gap-2 rounded-md border border-border bg-primary-foreground px-3 py-2 text-sm text-secondary-foreground whitespace-nowrap transition-[color,box-shadow] outline-none not-disabled:hover:border-border-normal not-disabled:active:border-border-strong focus-visible:bg-secondary focus-visible:text-secondary-foreground focus-visible:shadow-3xs disabled:cursor-not-allowed disabled:bg-input disabled:text-popover-foreground aria-invalid:border-destructive aria-invalid:ring-destructive/20 data-[placeholder]:text-card-foreground data-[size=default]:h-9 data-[size=sm]:h-8 *:data-[slot=select-value]:line-clamp-1 *:data-[slot=select-value]:flex *:data-[slot=select-value]:items-center *:data-[slot=select-value]:gap-2 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 [&_svg:not([class*='text-'])]:text-muted-foreground",
         className
       )}
       {...props}
@@ -74,7 +74,7 @@ function SelectContent({
         <SelectScrollUpButton />
         <SelectPrimitive.Viewport
           className={cn(
-            "p-1",
+            "p-1 [animation:scroll-shadow-mask_linear_both] [animation-timeline:scroll(self)]",
             position === "popper" &&
               "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)] scroll-my-1"
           )}
@@ -151,13 +151,11 @@ function SelectScrollUpButton({
     <SelectPrimitive.ScrollUpButton
       data-slot="select-scroll-up-button"
       className={cn(
-        "flex cursor-default items-center justify-center bg-popover py-1",
+        "flex cursor-default items-center justify-center py-1",
         className
       )}
       {...props}
-    >
-      <ChevronUpIcon className="size-4" />
-    </SelectPrimitive.ScrollUpButton>
+    />
   )
 }
 
@@ -169,13 +167,11 @@ function SelectScrollDownButton({
     <SelectPrimitive.ScrollDownButton
       data-slot="select-scroll-down-button"
       className={cn(
-        "flex cursor-default items-center justify-center bg-popover py-1",
+        "flex cursor-default items-center justify-center py-1",
         className
       )}
       {...props}
-    >
-      <ChevronDownIcon className="size-4" />
-    </SelectPrimitive.ScrollDownButton>
+    />
   )
 }
 
