ec5284d0e8b4f661e4fe9b97544dc77305b4ba36
devtimeless <dev@timeless.co>
2026-05-15 12:47:11 +0530

    fix(input): input, input group, text area focus change delay fix

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -5,17 +5,17 @@ import { cn } from "@/lib/utils"
 
 const textareaVariants = cva(
   [
-    "flex field-sizing-content w-full border leading-base font-normal tracking-normal text-secondary-foreground transition-colors outline-none placeholder:text-card-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input disabled:text-popover-foreground",
+    "flex field-sizing-content w-full border leading-base font-normal tracking-normal text-secondary-foreground outline-none placeholder:text-card-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input disabled:text-popover-foreground",
   ],
   {
     variants: {
       variant: {
         outline:
-          "border-border bg-primary-foreground not-disabled:hover:border-border-normal not-disabled:focus:bg-primary-foreground not-disabled:focus:shadow-3xs not-disabled:active:border-border-stronger disabled:bg-input disabled:text-popover-foreground data-valid:border-success-outline data-invalid:border-error-outline data-filled:border-border",
+          "border-border bg-primary-foreground not-disabled:hover:border-border-normal not-disabled:focus:bg-primary-foreground not-disabled:focus:shadow-3xs not-disabled:active:border-border-stronger disabled:bg-input disabled:text-popover-foreground data-filled:border-border data-invalid:border-error-outline data-valid:border-success-outline",
         subtle:
-          "border-transparent bg-secondary not-disabled:hover:bg-muted not-disabled:focus:bg-primary-foreground not-disabled:focus:shadow-3xs not-disabled:active:border-border-stronger not-disabled:active:bg-primary-foreground disabled:bg-input disabled:text-popover-foreground data-valid:bg-success data-invalid:bg-error data-filled:bg-secondary",
+          "border-transparent bg-secondary not-disabled:hover:bg-muted not-disabled:focus:bg-primary-foreground not-disabled:focus:shadow-3xs not-disabled:active:border-border-stronger not-disabled:active:bg-primary-foreground disabled:bg-input disabled:text-popover-foreground data-filled:bg-secondary data-invalid:bg-error data-valid:bg-success",
         ghost:
-          "border-transparent bg-transparent not-disabled:hover:bg-muted not-disabled:focus:border-transparent not-disabled:focus:bg-primary-foreground not-disabled:focus:shadow-3xs not-disabled:active:border-transparent! not-disabled:active:bg-primary-foreground disabled:bg-transparent disabled:text-popover-foreground data-valid:bg-success data-invalid:bg-error data-filled:bg-secondary",
+          "border-transparent bg-transparent not-disabled:hover:bg-muted not-disabled:focus:border-transparent not-disabled:focus:bg-primary-foreground not-disabled:focus:shadow-3xs not-disabled:active:border-transparent! not-disabled:active:bg-primary-foreground disabled:bg-transparent disabled:text-popover-foreground data-filled:bg-secondary data-invalid:bg-error data-valid:bg-success",
       },
       size: {
         sm: "min-h-15 rounded-md px-2 py-1.5 text-base",
@@ -35,8 +35,7 @@ function Textarea({
   variant,
   size,
   ...props
-}: React.ComponentProps<"textarea"> &
-  VariantProps<typeof textareaVariants>) {
+}: React.ComponentProps<"textarea"> & VariantProps<typeof textareaVariants>) {
   return (
     <textarea
       data-slot="textarea"
