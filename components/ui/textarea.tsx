068fd93c214cebe92fa4cb6cfc7cd99ca3c4b191
devtimeless <dev@timeless.co>
2026-05-27 15:14:31 +0530

    fix(textarea): remove border and add shadow to textarea component

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -5,17 +5,17 @@ import { cn } from "@/lib/utils"
 
 const textareaVariants = cva(
   [
-    "flex field-sizing-content w-full border leading-base font-normal tracking-normal text-secondary-foreground outline-none placeholder:text-card-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input disabled:text-popover-foreground",
+    "flex field-sizing-content w-full leading-base font-normal tracking-normal text-secondary-foreground outline-none placeholder:text-card-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input disabled:text-popover-foreground",
   ],
   {
     variants: {
       variant: {
         outline:
-          "border-border bg-background not-disabled:hover:border-border-normal not-disabled:focus:bg-background not-disabled:focus:shadow-3xs disabled:bg-input disabled:text-popover-foreground data-filled:border-border data-invalid:border-error-outline data-valid:border-success-outline",
+          "bg-background shadow-[0px_1px_1px_#0000000f,0px_0px_0px_1px_#00000012] group-data-[invalid=true]/field:shadow-[0px_1px_1px_#0000000f,0px_0px_0px_1px_var(--error-outline)]! not-disabled:hover:shadow-[0px_1px_1px_rgba(0,0,0,0.12),0px_0px_0px_1px_rgba(0,0,0,0.1)] not-disabled:focus:shadow-[0px_1px_1px_rgba(0,0,0,0.12),0px_0px_0px_1px_rgba(0,0,0,0.1)]! data-filled:shadow-[0px_1px_1px_#0000000f,0px_0px_0px_1px_var(--border)] data-invalid:shadow-[0px_1px_1px_#0000000f,0px_0px_0px_1px_var(--error-outline)] data-valid:shadow-[0px_1px_1px_#0000000f,0px_0px_0px_1px_var(--success-outline)] dark:shadow-[0px_1px_1px_rgba(0,0,0,0.08),0px_0px_0px_1px_rgba(255,255,255,0.1)] dark:not-disabled:hover:shadow-[0px_1px_1px_rgba(0,0,0,0.1),0px_0px_0px_1px_rgba(255,255,255,0.2)] dark:not-disabled:focus:shadow-[0px_1px_1px_rgba(0,0,0,0.1),0px_0px_0px_1px_rgba(255,255,255,0.2)]! disabled:bg-input disabled:text-popover-foreground",
         subtle:
-          "border-transparent bg-secondary not-disabled:hover:bg-muted not-disabled:focus:bg-background not-disabled:focus:shadow-3xs not-disabled:active:bg-background disabled:bg-input disabled:text-popover-foreground data-filled:bg-secondary data-invalid:bg-error data-valid:bg-success",
+          "bg-secondary not-disabled:hover:bg-muted not-disabled:focus:bg-background not-disabled:focus:shadow-[0px_1px_1px_rgba(0,0,0,0.12),0px_0px_0px_1px_rgba(0,0,0,0.1)]! not-disabled:active:bg-background dark:not-disabled:focus:shadow-[0px_1px_1px_rgba(0,0,0,0.1),0px_0px_0px_1px_rgba(255,255,255,0.2)]! disabled:bg-input disabled:text-popover-foreground data-filled:bg-secondary data-invalid:bg-error data-valid:bg-success",
         ghost:
-          "border-transparent bg-transparent not-disabled:hover:bg-muted not-disabled:focus:border-transparent not-disabled:focus:bg-background not-disabled:focus:shadow-3xs not-disabled:active:bg-primary-foreground disabled:bg-transparent disabled:text-popover-foreground data-filled:bg-secondary data-invalid:bg-error data-valid:bg-success",
+          "bg-transparent not-disabled:hover:bg-muted not-disabled:focus:bg-background not-disabled:focus:shadow-[0px_1px_1px_rgba(0,0,0,0.12),0px_0px_0px_1px_rgba(0,0,0,0.1)]! not-disabled:active:bg-primary-foreground dark:not-disabled:focus:shadow-[0px_1px_1px_rgba(0,0,0,0.1),0px_0px_0px_1px_rgba(255,255,255,0.2)]! disabled:bg-transparent disabled:text-popover-foreground data-filled:bg-secondary data-invalid:bg-error data-valid:bg-success",
       },
       size: {
         sm: "min-h-15 rounded-md px-2 py-1.5 text-base",
