662a799e8a43777e54324d2c1ebe67c73b80e865
devtimeless <dev@timeless.co>
2026-08-19 16:56:48 +0530

    fix(textarea): textarea component color fix done

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -5,17 +5,17 @@ import { cn } from "@/lib/utils"
 
 const textareaVariants = cva(
   [
-    "flex field-sizing-content w-full leading-lg font-normal tracking-normal text-secondary-foreground outline-none placeholder:text-card-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input disabled:text-popover-foreground",
+    "flex field-sizing-content w-full leading-lg font-normal tracking-normal text-secondary-foreground outline-none placeholder:text-[color-mix(in_oklch,var(--secondary-foreground),var(--background)_52%)] dark:placeholder:text-[color-mix(in_oklch,var(--secondary-foreground),var(--background)_37%)] disabled:placeholder:text-[color-mix(in_oklch,var(--secondary-foreground),var(--background)_74%)] dark:disabled:placeholder:text-[color-mix(in_oklch,var(--secondary-foreground),var(--background)_54%)] disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input disabled:text-[color-mix(in_oklch,var(--secondary-foreground),var(--background)_74%)] dark:disabled:text-[color-mix(in_oklch,var(--secondary-foreground),var(--background)_54%)]",
   ],
   {
     variants: {
       variant: {
         outline:
-          "bg-transparent shadow-default transition-shadow duration-150 group-data-[invalid=true]/field:shadow-[0px_1px_1px_#0000000f,0px_0px_0px_1px_var(--error-outline)]! not-disabled:hover:shadow-raised not-disabled:focus:shadow-raised! disabled:bg-transparent disabled:text-popover-foreground data-filled:shadow-[0px_1px_1px_#0000000f,0px_0px_0px_1px_var(--border)] data-invalid:shadow-[0px_1px_1px_#0000000f,0px_0px_0px_1px_var(--error-outline)] data-valid:shadow-[0px_1px_1px_#0000000f,0px_0px_0px_1px_var(--success-outline)]",
+          "bg-transparent shadow-default transition-shadow duration-150 group-data-[invalid=true]/field:shadow-[0px_1px_1px_#0000000f,0px_0px_0px_1px_var(--error-outline)]! not-disabled:hover:shadow-raised not-disabled:focus:shadow-raised! disabled:bg-transparent disabled:text-[color-mix(in_oklch,var(--secondary-foreground),var(--background)_74%)] dark:disabled:text-[color-mix(in_oklch,var(--secondary-foreground),var(--background)_54%)] data-filled:shadow-[0px_1px_1px_#0000000f,0px_0px_0px_1px_var(--border)] data-invalid:shadow-[0px_1px_1px_#0000000f,0px_0px_0px_1px_var(--error-outline)] data-valid:shadow-[0px_1px_1px_#0000000f,0px_0px_0px_1px_var(--success-outline)]",
         subtle:
-          "bg-secondary not-disabled:hover:bg-muted not-disabled:focus:bg-background not-disabled:focus:shadow-raised! not-disabled:active:bg-background disabled:bg-input disabled:text-popover-foreground data-filled:bg-secondary data-invalid:bg-error data-valid:bg-success",
+          "bg-secondary not-disabled:hover:bg-[color-mix(in_oklch,var(--secondary),black_2%)] dark:not-disabled:hover:bg-[color-mix(in_oklch,var(--secondary),white_5%)] not-disabled:focus:bg-background not-disabled:focus:shadow-raised! not-disabled:active:bg-background disabled:bg-input disabled:text-[color-mix(in_oklch,var(--secondary-foreground),var(--background)_74%)] dark:disabled:text-[color-mix(in_oklch,var(--secondary-foreground),var(--background)_54%)] data-filled:bg-secondary data-invalid:bg-error data-valid:bg-success",
         ghost:
-          "bg-transparent not-disabled:hover:bg-muted not-disabled:focus:bg-background not-disabled:focus:shadow-raised! not-disabled:active:bg-primary-foreground disabled:bg-transparent disabled:text-popover-foreground data-filled:bg-secondary data-invalid:bg-error data-valid:bg-success",
+          "bg-transparent not-disabled:hover:bg-[color-mix(in_oklch,var(--secondary),black_2%)] dark:not-disabled:hover:bg-[color-mix(in_oklch,var(--secondary),white_5%)] not-disabled:focus:bg-background not-disabled:focus:shadow-raised! not-disabled:active:bg-primary-foreground disabled:bg-transparent disabled:text-[color-mix(in_oklch,var(--secondary-foreground),var(--background)_74%)] dark:disabled:text-[color-mix(in_oklch,var(--secondary-foreground),var(--background)_54%)] data-filled:bg-secondary data-invalid:bg-error data-valid:bg-success",
       },
       size: {
         xs: "min-h-12 rounded-md px-2 py-1 text-sm",
