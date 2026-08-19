adc1f51f60129e5bafc4093778106468c77b07a2
devtimeless <dev@timeless.co>
2026-08-19 15:44:36 +0530

    fix(slider and sonner): slider and sonner component color fix

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -31,7 +31,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }}
       style={
         {
-          "--normal-bg": "var(--secondary-foreground)",
+          "--normal-bg":
+            "color-mix(in oklch, var(--primary), var(--background) 17%)",
           "--normal-text": "var(--primary-foreground)",
           "--normal-border": "var(--border)",
           "--border-radius": "var(--radius)",
