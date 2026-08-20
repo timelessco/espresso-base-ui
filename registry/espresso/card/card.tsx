99ec67096251d858a9c5c2d97158f07166425753
devtimeless <dev@timeless.co>
2026-08-20 19:13:46 +0530

    fix(registry): registry update

--- registry/espresso/card/card.tsx
+++ registry/espresso/card/card.tsx
@@ -4,12 +4,12 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const cardVariants = cva(
-  "group/card flex flex-col gap-4 overflow-hidden text-sm text-card-foreground shadow-default has-data-[slot=card-footer]:pb-0 has-[>img:first-child]:pt-0 data-[size=sm]:gap-3 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl",
+  "group/card flex flex-col gap-4 overflow-hidden text-sm text-card-foreground shadow-default has-data-[slot=card-footer]:pb-0 has-[>img:first-child]:pt-0 data-[size=sm]:gap-3 data-[size=lg]:gap-5 data-[size=xl]:gap-6 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl",
   {
     variants: {
       variant: {
         default:
-          "rounded-xl bg-card py-4 data-[size=sm]:py-3 data-[size=sm]:has-data-[slot=card-footer]:pb-0",
+          "rounded-xl bg-card py-4 data-[size=sm]:py-3 data-[size=lg]:py-5 data-[size=xl]:py-6",
         mail: "rounded-lg bg-card px-3 py-2.5",
         message: "rounded-lg bg-input px-3 py-2",
         call: "rounded-lg bg-card px-3 py-2.5",
@@ -23,11 +23,13 @@ const cardVariants = cva(
 
 function Card({
   className,
-  size = "default",
+  size = "md",
   variant,
   ...props
 }: React.ComponentProps<"div"> &
-  VariantProps<typeof cardVariants> & { size?: "default" | "sm" }) {
+  VariantProps<typeof cardVariants> & {
+    size?: "sm" | "md" | "lg" | "xl"
+  }) {
   return (
     <div
       data-slot="card"
@@ -44,7 +46,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card-header"
       className={cn(
-        "group/card-header @container/card-header grid auto-rows-min items-start gap-1 rounded-t-xl px-4 group-data-[size=sm]/card:px-3 has-data-[slot=card-action]:grid-cols-[1fr_auto] has-data-[slot=card-description]:grid-rows-[auto_auto] [.border-b]:pb-4 group-data-[size=sm]/card:[.border-b]:pb-3",
+        "group/card-header @container/card-header grid auto-rows-min items-start gap-1 rounded-t-xl px-4 group-data-[size=sm]/card:px-3 group-data-[size=lg]/card:px-5 group-data-[size=xl]/card:px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] has-data-[slot=card-description]:grid-rows-[auto_auto] [.border-b]:pb-4 group-data-[size=sm]/card:[.border-b]:pb-3 group-data-[size=lg]/card:[.border-b]:pb-5 group-data-[size=xl]/card:[.border-b]:pb-6",
         className
       )}
       {...props}
@@ -57,7 +59,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card-title"
       className={cn(
-        "text-base leading-snug font-medium text-foreground group-data-[size=sm]/card:text-sm",
+        "text-base leading-snug font-medium text-foreground group-data-[size=sm]/card:text-sm group-data-[size=lg]/card:text-lg group-data-[size=xl]/card:text-xl",
         className
       )}
       {...props}
@@ -92,7 +94,10 @@ function CardContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-content"
-      className={cn("px-4 group-data-[size=sm]/card:px-3", className)}
+      className={cn(
+        "px-4 group-data-[size=sm]/card:px-3 group-data-[size=lg]/card:px-5 group-data-[size=xl]/card:px-6",
+        className
+      )}
       {...props}
     />
   )
@@ -103,7 +108,7 @@ function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card-footer"
       className={cn(
-        "flex items-center rounded-b-xl p-4 group-data-[size=sm]/card:p-3",
+        "flex items-center rounded-b-xl p-4 group-data-[size=sm]/card:p-3 group-data-[size=lg]/card:p-5 group-data-[size=xl]/card:p-6",
         className
       )}
       {...props}
